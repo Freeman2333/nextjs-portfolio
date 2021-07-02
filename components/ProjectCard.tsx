c63a62b52c6eb4e3232dfebdc50c9ea320bdb612
s.popov <s.popov@indevlab.com>
2021-07-02 15:42:20 +0300

    projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -38,7 +38,18 @@ const ProjectCard: FunctionComponent<{
                             </a>
                         </div>
                     </div>
-                    <div></div>
+                    <div>
+                        <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
+                        <h3 className="mb-3 font-medium">{description}</h3>
+                        <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+                            {key_techs.map(tech=>(
+                                <span key={tech} className="px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounde-sm">{tech}</span>
+                            ))}
+                        </div>
+                    </div>
+                    <button className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-dark-200" onClick={()=>setShowDetail(false)}>
+                        <MdClose size={30} />
+                    </button>
                 </div>
             )}
         </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
         layout="intrinsic"
         quality="100"
       />
-      <h3 className="my-4 text-3xl font-medium tracking-wider ">
+      <h3 className="my-4 text-3xl font-medium tracking-wider kaushan">
         <span className="text-green">Popov</span> Serhii
       </h3>
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
